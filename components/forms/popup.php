25be6de2afaf8d41db164f0a9612e80be41c3dbb
ajay <ajaykmankani66@gmail.com>
2022-12-23 18:34:08 +0530

    a

--- components/forms/popup.php
+++ components/forms/popup.php
@@ -287,7 +287,7 @@
             <p class="sign-up">Don't have an account? <a href="#">Sign up now</a></p>
         </div>
         <div class="modal-right">
-            <img src="https://images.unsplash.com/photo-1512486130939-2c4f79935e4f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=dfd2ec5a01006fd8c4d7592a381d3776&auto=format&fit=crop&w=1000&q=80" alt="">
+            <img src="https://img.freepik.com/free-photo/coins-bottles-with-trading-graph_1150-17751.jpg?w=1380&t=st=1671800598~exp=1671801198~hmac=f9d118fa6cde9883d8830842a18b0dcbf47458e39368fd108d8458f1d8f41287" alt="">
         </div>
         <button class="icon-button close-button">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
