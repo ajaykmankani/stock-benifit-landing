e687002ee6814e724a2ad960082566fe447ae063
ajay <ajaykmankani66@gmail.com>
2022-12-23 20:46:13 +0530

    a

---
+++ privacy.php
@@ -0,0 +1,28 @@
+<?php include('containers\header.php'); ?>
+<?php include('containers\navbar.php'); ?>
+<div class="container">
+    <h2>Privacy Policy</h2>
+    <section>
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            <p>At&nbsp;<?= $brand_name ?>&nbsp;&ndash; Investment Advisor we respect your privacy. To better protect your privacy we provide this notice explaining our online information practices and the choices you can make about the way your information is collected and used.</p>
+                            <p>We gather certain information that is provided by you to the Web Site. We believe that by collecting this information We will be able to provide you the best of our services to assist our own team in efficiently reaching an appropriate audience.</p>
+                            <p>The privacy and protection of your data and information provided to us is of vital importance. We shall ensure to safeguard the security and confidentiality of any information you share with us. Any personally identifiable information of the customers obtained by us shall not be used or shared other than for the purposes to which the customers consents. However security and confidentiality of information cannot be guaranteed cent percent. Hence despite our utmost efforts to protect your personal information, we cannot warrant the security of any information you transmit to us through our online services. Such transmission of your personal information is done at your own risk.</p>
+                            <p>In addition to the service provided to you, your information (mobile number, E-mail ID etc.) can be brought in use for sending you newsletters, surveys, contest information, or information about any new services of the company which will be for your benefit and while subscribing for our services, you agree that&nbsp;<?= $brand_name ?>&nbsp;&ndash; Investment Advisor has the right to do so.</p>
+                            <p>By filling out the 'Inquiry form' on this website, you agree to provide us your valid mobile number. Also by providing your mobile number, you provide us the consent to call you on the number provided by you and send SMS on your mobile number. You provide your consent for us to call / SMS you on your mobile number even if it is registered on the National 'Do Not Disturb' registry.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<style>
+    footer#footer {
+        position: inherit !important;
+    }
+</style>
+<?php include('containers/footer.php'); ?>
